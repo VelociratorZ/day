d8c1fb1ea90ad20ac911f5207ba8f262c1cd13ce
Nathaneal Tan <nathan3al@gmail.com>
2025-02-04 23:41:32 +1100

    fina

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let list = document.getElementById("picture")
 console.log(list)
 const num = 52;
-const titles = ["OTW to Watson's Bay","Valentine's to Watson's Bay"]
+const titles = ["OTW to Watson's Bay","Valentine's to Watson's Bay","Our first stop at during our first Trip",
+    "Silhouette Pic","Temple Selfie","Temple Selfie 2","With Bababooey",
+    "Wollonggong Beach","Before our dinner date at Wollongong","Night Heart","Weird Mouth","Smileyss Data",
+    "Miniso?","Split Face","Tongue","First eat out with the kids","Family Dinnerrrrrrrrr"," Cat Day","OTW Cafe",
+    "Cat 1", "Cat 2","Opera Views","Octopussyyy",
+    "Lunch with Dad :)","After lunch Walk","Boyfriend Day","Big spending","Golf Vibes","Winnerrrssss","Gardens",
+    "To ETTALONG BEACH","DUCK", "Teahouse","Trauma","Pre Date","New Day Walks","Matching Watches","Merry Christmas",
+    "Two Teas","Martin's Place Tree","Japan Shrine","Your Name","Your Name 2","Shibuya","Team Lab Vibes","No 2","Final Market",
+    "Family Trip Painting", "Your Fav Pic","On the way backk","Yumchaa"," CHINESE NEW YEAR"
+]
 
 const subtitle = ["Our first freey ride together","I made us go on a hike during fire weather"]
 for(let i = 0; i<num; i++){
@@ -14,7 +23,6 @@ for(let i = 0; i<num; i++){
                         <h2>${titles[i]}</h2>
                         <div class="desc">
                             <img class="portrait" src="images/${i+2}.JPG">
-                            <p>${subtitle[i]}</p>
                         </div>
                     </div>
                 </div>
